d786251f977c1f8a717508a40bf5408bf125e13b
CarterFitzsimons <cfitzsimons1@mail.csuchico.edu>
2018-02-22 11:05:16 -0800

    Lets the user add up to 5 items to the list.

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+//Author: Carter Fitzsimons
 #include<iostream>
 #include<string>
 using namespace std;
@@ -7,13 +8,29 @@ int main()
   string list[5]; //array of 5 strings
   int numItems = 0;
   char input;
+  string newItem;
 
-  cout<<"\n==GROCERY LIST MANAGER==";
-  cout<<"\nEnter your choice: ";
-  cout<<"\n (A)dd an item";
-  cout<<"\n (Q)uit";
-  cout<<"\nYour choice (A/Q): ";
-  cin>>input;
+  do{
+    cout<<"\n==GROCERY LIST MANAGER==";
+    cout<<"\nEnter your choice: ";
+    cout<<"\n (A)dd an item";
+    cout<<"\n (Q)uit";
+    cout<<"\nYour choice (A/Q): ";
+    cin>>input;
+    
+    if(input == 'a' || input == 'A'){
+        cout<<"What is the item?";
+        cin>>newItem;
+        if(numItems <= 5){
+            string list[numItems] = newItem;
+            numItems = numItems + 1;
+        }
+        if(numItems > 5) {
+            cout<<"You'll need a bigger list!"; 
+        }
+    }
+  }
+  while(input != 'q' && input != 'Q');
 
   return 0;
 }
